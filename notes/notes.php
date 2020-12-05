a4804a3962e40bfb6b527facd6fbcd42b6935066
Boozy-Cat <52620643+Boozy-Cat@users.noreply.github.com>
2020-12-05 15:43:05 +0200

    Added new folder and changed html to php

---
+++ notes/notes.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include '../php/stylesheet.php'?>
+    <title>My notes</title>
+</head>
+<body>
+    <?php include '../php/header.php'?>
+    <?php include '../php/navigation_bar.php'?>
+
+    <ul class="note-list">
+        <li><a href="mac-table.php">Table of MAC-Adresses</a></li>
+        <li><a href="neighbourhoods-in-home.php">Peoples</a></li>
+    </ul>
+
+    
+    <br>
+    <?php include '../php/footer.php'?>
+</body>
+</html>
