a541e367ba7e7a89d4d9b6e40241e6f432abd28a
Boozy-Cat <52620643+Boozy-Cat@users.noreply.github.com>
2020-12-05 15:43:43 +0200

    Add two pages with tables

---
+++ notes/mac-table.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include '../php/stylesheet.php'?>
+    <title>MAC-Adress</title>
+</head>
+<body>
+    <?php include '../php/header.php'?>
+    <?php include '../php/navigation_bar.php'?>
+
+<!-- 
+    //todo place for search panel
+
+
+
+ -->
+
+    <table id="TABLE_MAC">
+        <tr>
+            <th id="tb_mac_name">ID</th>
+            <th>Name</th>
+            <th>Device</th>
+            <th>MAC-Adress</th>
+        </tr>
+        <tr>
+            <td id="tb_mac_name">1</td>
+            <td>Леха мойка</td>
+            <td>RedmiNote9Pro-RedmiN</td>
+            <td style="width:280px">BC-7F-A4-3A-71-37</td>
+        </tr>
+        <tr id="even_row">
+            <td id="tb_mac_name">2</td>
+            <td>???</td>
+            <td>Honor_8A-d7b78d0640237792</td>
+            <td style="width:160px">24-FB-65-1B-59-31</td>
+        </tr>
+        <tr>
+            <td id="tb_mac_name">3</td>
+            <td>???</td>
+            <td>M2003J15SC-RedmiNote</td>
+            <td style="width:160px">98-F6-21-73-01-73</td>
+        </tr>
+        <tr id="even_row">
+            <td id="tb_mac_name">4</td>
+            <td>Настя</td>
+            <td>RedmiNote8T-RedmiNot</td>
+            <td style="width:160px">90-78-B2-8F-6B-3F</td>
+        </tr>
+        <tr>
+            <td id="tb_mac_name">5</td>
+            <td>Ваня</td>
+            <td>Galaxy-A20e</td>
+            <td style="width:160px">A8-87-B3-CA-C9-E6</td>
+        </tr>
+        <tr id="even_row">
+            <td id="tb_mac_name">6</td>
+            <td>???</td>
+            <td>Unknown</td>
+            <td style="width:160px">48-FD-A3-78-B9-4B</td>
+        </tr>
+        <tr>
+            <td id="tb_mac_name">7</td>
+            <td>Me_sony</td>
+            <td>android-4d0c6d74850c6b3c</td>
+            <td style="width:160px">40-40-A7-5F-B5-83</td>
+        </tr>
+        <tr id="even_row">
+            <td id="tb_mac_name">8</td>
+            <td>???</td>
+            <td>���-��</td>
+            <td style="width:160px">E0-B9-A5-F6-0B-CE</td>
+        </tr>
+        <tr>
+            <td id="tb_mac_name">9</td>
+            <td>haoki-pad</td>
+            <td>DSKTP-1D59A02N9</td>
+            <td style="width:160px">30-52-CB-70-4A-93</td>
+        </tr>
+        <tr id="even_row">
+            <td id="tb_mac_name">10</td>
+            <td>???</td>
+            <td>android-a2d5655d075ad9d1</td>
+            <td style="width:160px">00-EC-A4-91-A0-38</td>
+        </tr>
+        <tr>
+            <td id="tb_mac_name">11</td>
+            <td>???</td>
+            <td>android-4bffd4981cc22743</td>
+            <td style="width:160px">00-00-7E-61-FB-7E</td>
+        </tr>
+        <tr id="even_row">
+            <td id="tb_mac_name">12</td>
+            <td>???</td>
+            <td>android-ac1148aa93c3af07</td>
+            <td style="width:160px">2C-FD-AB-8A-23-2C</td>
+        </tr>
+        <tr>
+            <td id="tb_mac_name">13</td>
+            <td>???</td>
+            <td>HUAWEI_P_smart-6b136e4334</td>
+            <td>B4-86-55-35-07-66</td>
+        </tr>
+        <tr id="even_row">
+            <td id="tb_mac_name">14</td>
+            <td>???</td>
+            <td>RedmiNote7-Redmi7</td>
+            <td>20-34-FB-F1-A0-3A</td>
+        </tr>
+        <tr>
+            <td id="tb_mac_name">15</td>
+            <td>Sania(MC Kefir) <a href="#  ">♫</a></td>
+            <td>meizu</td>
+            <td>FC-53-9E-BE-35-E6</td>
+        </tr>
+        <tr id="even_row">
+            <td id="tb_mac_name">16</td>
+            <td>Ваня(Рассыпанный Сахар)</td>
+            <td>Galaxy-A6-polzovatela-Ivan</td>
+            <td>80-4E-70-D5-47-1C</td>
+        </tr>
+        <tr>
+            <td id="tb_mac_name">17</td>
+            <td>Max</td>
+            <td>Unknown</td>
+            <td>7C-D6-61-97-4E-39</td>
+        </tr>
+        <tr id="even_row">
+            <td id="tb_mac_name">18</td>
+            <td>???</td>
+            <td>Galaxy-J7-Neo</td>
+            <td>34-2D-0D-E5-E5-3C</td>
+        </tr>
+        <tr>
+            <td id="tb_mac_name">19</td>
+            <td>???</td>
+            <td>Unknown</td>
+            <td>C0-10-B1-32-BE-D0</td>
+        </tr>
+        <tr id="even_row">
+            <td id="tb_mac_name">20</td>
+            <td>Roma</td>
+            <td>Galaxy-A10s-polzovatela-Rasad</td>
+            <td>2E-C7-99-DA-61-1E</td>
+        </tr>
+        <tr>
+            <td id="tb_mac_name">21</td>
+            <td>Леша</td>
+            <td>MEIZU-M5s</td>
+            <td>FC-53-9E-A5-DA-5A</td>
+        </tr>
+        <tr id='even_row'>
+            <td id='tb_mac_name'>22</td>
+            <td>???</td>
+            <td>android-452d14bb9b5f510</td>
+            <td>78-F7-BE-DC-99-75</td>
+        </tr>
+        <tr>
+            <td id='tb_mac_name'>23</td>
+            <td>???</td>
+            <td>android-1f0f0587d9e2477</td>
+            <td>64-DB-43-4C-EC-D7</td>
+        </tr>
+    </table>
+    <br>
+    <?php include '../php/footer.php'?>
+</body>
+</html>
