b6f4d3efc0b446994c571405b74a5e22e9cde9b8
Boozy-Cat <52620643+Boozy-Cat@users.noreply.github.com>
2020-12-05 15:27:30 +0200

    Create new file and fill it with templates

---
+++ projects.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset='utf-8'>
+        <title>My projects</title>
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+        <?php include 'php/stylesheet.php'?>
+        <!-- <script src='main.js'></script> -->
+    </head>
+    <body>
+        <?php include 'php/header.php'?>
+        <?php include 'php/navigation_bar.php'?>
+        <br>
+        <?php include 'php/footer.php'?>
+    </body>
+</html>
