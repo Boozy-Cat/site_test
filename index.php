08843ba6001e1c966ac322a603e319d29993ee3f
Boozy-Cat <52620643+Boozy-Cat@users.noreply.github.com>
2020-12-05 15:16:29 +0200

    Convert 'index' file from html to php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Intro</title>
+        <?php include 'php/stylesheet.php'?>
+    </head>
+    <body>
+        <?php include 'php/header.php'?>
+        <?php include 'php/navigation_bar.php'?>
+        <br>
+        <main>
+            <p>Hello my nick is Haoki. I like anime, ranobe. <br>
+                So with that I decide to start learn japanese language and culture.
+            </p>
+        </main><br>
+        <?php include 'php/footer.php'?>
+    </body>
+</html>
